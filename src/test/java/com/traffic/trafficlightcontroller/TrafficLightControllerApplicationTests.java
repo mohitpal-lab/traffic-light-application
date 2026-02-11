cab585ffad18e2141d0f44d883e633b115057ab0
mohitpal-lab <68553886+mohitpal-lab@users.noreply.github.com>
2026-02-11 20:30:36 +0530

    Initial commit: set up application

---
+++ src/test/java/com/traffic/trafficlightcontroller/TrafficLightControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traffic.trafficlightcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficLightControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
